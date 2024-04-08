e8912861e4d80965089e4427f76659f3ee368d3e
Eric Santos <ericshantos@gmail.com>
2024-04-07 22:15:03 -0300

    docs(docs): Cria o repositório de documentação
    - Inicia a arquitetura da documentação com a biblioteca sphinx.

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'API-meme-aleatorio'
+copyright = '2024, Eric dos Santos'
+author = 'Eric dos Santos'
+release = 'Uma API Rest Flask que retorna memes aleatórios do site memedroid'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt-BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
